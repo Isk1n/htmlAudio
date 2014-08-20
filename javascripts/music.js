5b545fc6c65f54968d60c36cfa7735af27093905
Isk1n <Isk1n@users.noreply.github.com>
2014-08-20 11:00:08 +0300

    Add some new music with credits from incompetech

--- javascripts/music.js
+++ javascripts/music.js
@@ -75,16 +75,20 @@ function prevMusic() {
 }
 
 var musicUrls = {
-    kiwi6: [
-        'http://k007.kiwi6.com/hotlink/ijr85aciol/Kelly_Clarkson_-_My_Life_Would_Suck_Without_You_-_Piano_Cover.mp3',
-        'http://k007.kiwi6.com/hotlink/ts1mwp3a1c/Accelerated_Heartbeat_Rain_Soaked_Mix_.mp3',
-        'http://k007.kiwi6.com/hotlink/3k676w8niy/Daft_Punk_-_Get_Lucky_Ask_You_In_Gray_8-BIT_.mp3',
-        'http://k007.kiwi6.com/hotlink/i5jbbmvxjr/Ballad_of_Fallen_Angels.mp3',
-        'http://k007.kiwi6.com/hotlink/wv3lwe5aen/Electric_Fence_feat._Shift_-_La_carciuma_de_la_drum_dj_waveSound_vs_leftside_remix_.mp3',
-        'http://k007.kiwi6.com/hotlink/pu1uer096y/Echo_Of_MyYouth_Utah_Raptors_.mp3'
+    aggresive: [
+        'http://incompetech.com/music/royalty-free/mp3-royaltyfree/Black%20Vortex.mp3',
+        'http://incompetech.com/music/royalty-free/mp3-royaltyfree/The%20Complex.mp3',
+        'http://incompetech.com/music/royalty-free/mp3-royaltyfree/Go%20Cart.mp3'
     ],
-    aggrotech: [
-        'http://promodj.com/download/3375041/Electrocore%20%E2%80%93%20The%20Entity%20and%20the%20Fault%20%28promodj.com%29.mp3',
+    dark: [
+        'http://incompetech.com/music/royalty-free/mp3-royaltyfree/Reign%20Supreme.mp3',
+        'http://incompetech.com/music/royalty-free/mp3-royaltyfree/Drums%20of%20the%20Deep.mp3',
+        'http://incompetech.com/music/royalty-free/mp3-royaltyfree/The%20Builder.mp3'
+    ],
+    intense: [
+        'http://incompetech.com/music/royalty-free/mp3-royaltyfree/Undaunted.mp3',
+        'http://incompetech.com/music/royalty-free/mp3-royaltyfree/Unity.mp3',
+        'http://incompetech.com/music/royalty-free/mp3-royaltyfree/Dark%20Fog.mp3'
     ]
 };
 
